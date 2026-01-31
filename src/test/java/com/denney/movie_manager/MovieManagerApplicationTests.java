29ed0c0629a16017fd01eb2f219b54d232965f8f
Mike Denney <mike.a.denney@gmail.com>
2026-01-30 21:31:42 -0600

    initial commit with setup files

---
+++ src/test/java/com/denney/movie_manager/MovieManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denney.movie_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

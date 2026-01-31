0e133946528ddcd049cabed1541accd2f56b700c
Mike Denney <mike.a.denney@gmail.com>
2026-01-30 22:59:22 -0600

    added controller, repository and class definitions

---
+++ src/main/java/com/denney/movie_manager/controllers/MovieController.java
@@ -0,0 +1,11 @@
+package com.denney.movie_manager.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reviews")
+public class MovieController {
+
+}
---
+++ src/main/java/com/denney/movie_manager/models/Movie.java
@@ -0,0 +1,42 @@
+package com.denney.movie_manager.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private int reviewStars;
+
+    public Movie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReviewStars() {
+        return reviewStars;
+    }
+
+    public void setReviewStars(int reviewStars) {
+        this.reviewStars = reviewStars;
+    }
+}

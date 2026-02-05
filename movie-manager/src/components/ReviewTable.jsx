ee6ef3b685bacf2840210a12baee5f4b3de9f421
MostLikelyNotHuman <55810556+MostLikelyNotHuman@users.noreply.github.com>
2026-02-04 19:40:52 -0600

    added review filter functionality

--- movie-manager/src/components/ReviewTable.jsx
+++ movie-manager/src/components/ReviewTable.jsx
@@ -1,7 +1,71 @@
-const ReviewTable = ({reviews}) => {
+import { useState } from "react";
+import Button from "./Button"
 
-    return <table>
+const ReviewTable = ({reviews, isLoading, handleSubmit, handleChange}) => {
+
+    const [ reviewFilter, setReviewFilter ] = useState(0);
+
+    const filterTable = () => {
+        if (!reviewFilter) {
+            return (
+                reviews.map((movie, index) => (
+                    <tr key={movie.id || index}>
+                    <td>{movie.title}</td>
+                    <td>{movie.reviewStars}</td>
+                    <td>{movie.description}</td>
+                </tr>
+            )
+        ))} else if (reviewFilter === "0") {
+            return (
+                reviews.map((movie, index)=> (
+                    <tr key={movie.id || index}>
+                    <td>{movie.title}</td>
+                    <td>{movie.reviewStars}</td>
+                    <td>{movie.description}</td>
+                </tr>
+            )))
+        } else {
+            return (
+                reviews.filter(b => b.reviewStars == reviewFilter).map((movie, index) => (
+                    <tr key={movie.id || index}>
+                    <td>{movie.title}</td>
+                    <td>{movie.reviewStars}</td>
+                    <td>{movie.description}</td>
+                </tr>
+            )))
+        }
+    }
+
+
+
+
+    return <>
         <caption>Your Reviews</caption>
+        <label htmlFor="reviewFilter">Load Reviews by Rating: </label>
+        <select
+            id="reviewFilter"
+            name="reviewFilter"
+            value={reviewFilter}
+            // todo: update to run filter method instead of adding input data
+            onChange={(e) => {
+                setReviewFilter(e.target.value);
+            }}
+        >
+            <option value="0"></option>
+            <option value="1">⭐</option>
+            <option value="2">⭐⭐</option>
+            <option value="3">⭐⭐⭐</option>
+            <option value="4">⭐⭐⭐⭐</option>
+            <option value="5">⭐⭐⭐⭐⭐</option>
+        </select>
+        {/* <Button
+            id="submit-filter"
+            type="submit"
+            label={isLoading ? "Loading..." : "Load"}
+            disabled={isLoading}
+            handleClick={handleSubmit}
+        /> */}
+        <table>
         <thead>
             <tr>
                 <th>Title</th>
@@ -10,15 +74,17 @@ const ReviewTable = ({reviews}) => {
             </tr>
         </thead>
         <tbody>
-            {reviews.map((movie, index) => (
+            {/* {reviews.map((movie, index) => (
                 <tr key={movie.id || index}>
                     <td>{movie.title}</td>
                     <td>{movie.reviewStars}</td>
                     <td>{movie.description}</td>
                 </tr>
-            ))}
+            ))} */}
+            {filterTable()}
         </tbody>
     </table>
+    </>
 }
 
 export default ReviewTable
